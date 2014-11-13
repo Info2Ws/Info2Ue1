c648a03a7ba0654ce9b73917bb5c86f756f7227a
ZakRyze <ZakRyze@users.noreply.github.com>
2014-11-13 23:47:16 +0100

    Create datetime.h

---
+++ datetime.h
@@ -0,0 +1,30 @@
+#ifndef _DATETIME_H_
+#define _DATETIME_H_
+
+/******************************************************
+* FUNKTION: printTime
+*-----------------------------------------------------
+* BESCHREIBUNG: Ausgabe des der Dauer eines Titels
+*				
+* PARAMETER: Zeiger auf eine Zeichenkette
+*			  
+*			 
+* ERGEBNISTYP: void
+* Rückgabewert 0 bei Erfolg
+* 1 bei Fehlern
+*-----------------------------------------------------
+* ERSTELLT VON: 
+*           AM: 22.10.2014
+* ÄNDERUNGEN  : 28.10.2014
+******************************************************/
+
+
+/** Funktions-Pototypen **/
+
+
+    void printTime(TTime *Zeit);
+    void getTime(char *Text, TTime *Zeit);
+    void splitstring (TTime *Zeit, char c, char ***arr);
+    int checkTime (TTime *Zeit , int legit, char *strTime);
+#endif
+     
