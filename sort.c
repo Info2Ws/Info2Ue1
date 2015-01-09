c758dc3c7009849d4d2b1c9fc40affc5bb387f26
B4L4NCE <crimson@web.de>
2015-01-09 01:27:52 +0100

    create sort.c and sort.h + update media.c

---
+++ sort.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tools.h"
+#include "media.h"
+#include "datetime.h"
+#include "datastructure.h"
+#include "database.h"
+#include "sort.h"
+
+/**********************************************************
+ * FUNCTION:        sort 
+ * --------------------------------------------------------
+ * DESCRIPTION:
+ *********************************************************/
+ 
+ int sort(){
+	
+ 
+ }
---
+++ sort.h
@@ -0,0 +1,11 @@
+#ifndef _SORT_H_
+#define _SORT_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "datastructure.h"
+
+/* Funktions Prototypen */
+int sort();
+
+#endif
