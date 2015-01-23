9ef40c3abfdd353bfeada6b139a6816cfc48759c
ZakRyze <ZakRyze@users.noreply.github.com>
2015-01-23 15:06:40 +0100

    Create list.h

---
+++ list.h
@@ -0,0 +1,14 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "datastructure.h"
+
+/* Funktionsprototypen */
+void insertInMediaList();
+void removeFromMediaList();
+int cmpMediatitle();
+
+
+#endif
